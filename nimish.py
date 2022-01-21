dd4adb5a186c661e683262c20a8c7427ccff4d4f
Nimish Patel <patelnimish6@gmail.com>
2022-01-20 21:49:44 -0500

    MY PROJECT

--- nimish.py
+++ nimish.py
@@ -22,28 +22,28 @@ import pandas as pd
 # 1. Import data
 titanic = pd.read_csv('titanic.csv')
     
-titanic_soham = pd.DataFrame(titanic)
+titanic_nimish = pd.DataFrame(titanic)
 
 
 # In[154]:
 
 
-titanic_soham.head(3)
+titanic_nimish.head(3)
 
 
 # In[155]:
 
 
 
-print(titanic_soham.shape)
-titanic_soham.dtypes
+print(titanic_nimish.shape)
+titanic_nimish.dtypes
 
 
 # In[156]:
 
 
 columnList = []
-for column in titanic_soham.columns:
+for column in titanic_nimish.columns:
     columnList.append(column)
 
 
@@ -52,13 +52,13 @@ for column in titanic_soham.columns:
 
 #Missing values
 for column in columnList:
-    print("The number of missing values in "+column+" is ", titanic_soham[column].isnull().sum())
+    print("The number of missing values in "+column+" is ", titanic_nimish[column].isnull().sum())
 
 
 # In[158]:
 
 
-titanic_soham.head(10)
+titanic_nimish.head(10)
 
 
 # In[159]:
@@ -67,15 +67,15 @@ titanic_soham.head(10)
 # print unique values of Sex and Pclass column
 unique_values = ['Sex', 'Pclass']
 for column in unique_values:
-    print("The unique values in "+column+" are: ", titanic_soham[column].unique())
+    print("The unique values in "+column+" are: ", titanic_nimish[column].unique())
 
 
 # In[2]:
 
 
 #a.	A bar chart showing the # of survived versus the passenger class
-pd.crosstab(titanic_soham.Pclass, titanic_soham.Survived).plot(kind='bar')
-plt.title('"sohams" Survived vs Passenger Class')
+pd.crosstab(titanic_nimish.Pclass, titanic_nimish.Survived).plot(kind='bar')
+plt.title('"nimishtitanic_nimishs" Survived vs Passenger Class')
 plt.xlabel('Passenger Class')
 plt.ylabel('Survived')
 
@@ -85,8 +85,8 @@ plt.ylabel('Survived')
 
 
 #b.	A bar chart showing the # of survived versus gender
-pd.crosstab(titanic_soham.Sex, titanic_soham.Survived).plot(kind="bar")
-plt.title("sohams Survived vs Gender")
+pd.crosstab(titanic_nimish.Sex, titanic_nimish.Survived).plot(kind="bar")
+plt.title("nimishtitanic_nimishs Survived vs Gender")
 plt.xlabel('Gender')
 plt.ylabel('Survived')
 
@@ -96,14 +96,14 @@ plt.ylabel('Survived')
 
 # 2.scatter matrix to plot the relationships between the number of survived 
 pd.DataFrame(np.random.randn(1000, 5), columns =['Sex', 'Pclass', 'SibSp', 'Fare', 'Parch'])
-pd.plotting.scatter_matrix(titanic_soham, alpha=0.2)
+pd.plotting.scatter_matrix(titanic_nimish, alpha=0.2)
 
 
 # In[163]:
 
 
 #Dropping the columns with not enough data
-titanic_soham = titanic_soham.drop(['Name', 'Ticket', 'Cabin'], axis=1)
+titanic_nimish = titanic_nimish.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
 
 # In[164]:
@@ -111,16 +111,16 @@ titanic_soham = titanic_soham.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
 # Dummy variables
 
-titanic_soham = pd.get_dummies(titanic_soham, columns=['Sex'], prefix='Sex')
-titanic_soham
+titanic_nimish = pd.get_dummies(titanic_nimish, columns=['Sex'], prefix='Sex')
+titanic_nimish
 
 
 # In[165]:
 
 
 
-titanic_soham = pd.get_dummies(titanic_soham, columns=['Embarked'], prefix='Embarked')
-titanic_soham
+titanic_nimish = pd.get_dummies(titanic_nimish, columns=['Embarked'], prefix='Embarked')
+titanic_nimish
 
 
 # In[166]:
@@ -128,21 +128,21 @@ titanic_soham
 
 #Replace the missing values in the Age with the mean of the age
 
-titanic_soham['Age'] = titanic_soham['Age'].fillna(titanic_soham['Age'].mean())
-titanic_soham
+titanic_nimish['Age'] = titanic_nimish['Age'].fillna(titanic_nimish['Age'].mean())
+titanic_nimish
 
 
 # In[167]:
 
 
-for col in titanic_soham.columns:
-    titanic_soham[col] = pd.Series(titanic_soham[col],dtype=np.dtype("float"))
+for col in titanic_nimish.columns:
+    titanic_nimish[col] = pd.Series(titanic_nimish[col],dtype=np.dtype("float"))
 
 
 # In[168]:
 
 
-titanic_soham.info()
+titanic_nimish.info()
 
 
 # In[169]:
@@ -157,20 +157,20 @@ def normalizes(df):
 # In[187]:
 
 
-titanic_soham = normalizes(titanic_soham)
-print(titanic_soham)
+titanic_nimish = normalizes(titanic_nimish)
+print(titanic_nimish)
 
 
 # In[171]:
 
 
-hist = titanic_soham.hist(figsize=(9,10))
+hist = titanic_nimish.hist(figsize=(9,10))
 
 
 # In[172]:
 
 
-titanic_soham.columns
+titanic_nimish.columns
 
 
 # In[173]:
@@ -182,15 +182,15 @@ cols = ['PassengerId', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex_female', 
 # In[174]:
 
 
-X = titanic_soham[cols]
-y = titanic_soham['Survived']
+X = titanic_nimish[cols]
+y = titanic_nimish['Survived']
 
 
 # In[175]:
 
 
 from sklearn.model_selection import train_test_split
-X_train_soham,X_test_soham,y_train_soham,y_test_soham=train_test_split(X,y,test_size=0.3,random_state=2)
+X_train_nimishtitanic_nimish,X_test_nimishtitanic_nimish,y_train_nimishtitanic_nimish,y_test_nimishtitanic_nimish=train_test_split(X,y,test_size=0.3,random_state=2)
 
 
 # In[176]:
@@ -200,37 +200,37 @@ X_train_soham,X_test_soham,y_train_soham,y_test_soham=train_test_split(X,y,test_
 from sklearn.linear_model import LogisticRegression
 
 # instantiate the model (using the default parameters)
-soham_model = LogisticRegression(solver='lbfgs', multi_class='multinomial')  
+nimishtitanic_nimish_model = LogisticRegression(solver='lbfgs', multi_class='multinomial')  
 
 # fit the model with data
-soham_model.fit(X_train_soham,y_train_soham)
+nimishtitanic_nimish_model.fit(X_train_nimishtitanic_nimish,y_train_nimishtitanic_nimish)
 
 
 # In[177]:
 
 
-probs = soham_model.predict_proba(X_test_soham)
+probs = nimishtitanic_nimish_model.predict_proba(X_test_nimishtitanic_nimish)
 probs
 
 
 # In[178]:
 
 
-predicted = soham_model.predict(X_test_soham)
+predicted = nimishtitanic_nimish_model.predict(X_test_nimishtitanic_nimish)
 predicted
 
 
 # In[179]:
 
 
-accuracy_score = metrics.accuracy_score(y_test_soham, predicted)
+accuracy_score = metrics.accuracy_score(y_test_nimishtitanic_nimish, predicted)
 accuracy_score*100
 
 
 # In[180]:
 
 
-metrics.confusion_matrix(y_test_soham, predicted) 
+metrics.confusion_matrix(y_test_nimishtitanic_nimish, predicted) 
 
 
 # In[181]:
@@ -239,7 +239,7 @@ metrics.confusion_matrix(y_test_soham, predicted)
 from sklearn.model_selection import cross_val_score, cross_validate
 modelCV = LogisticRegression()
 scoring = {'accuracy': 'accuracy', 'log_loss': 'neg_log_loss', 'auc': 'roc_auc'}
-results = cross_validate(modelCV, X_train_soham, y_train_soham, cv=10, scoring=list(scoring.values()), 
+results = cross_validate(modelCV, X_train_nimishtitanic_nimish, y_train_nimishtitanic_nimish, cv=10, scoring=list(scoring.values()), 
                          return_train_score=False)
 
 
@@ -261,7 +261,7 @@ results['test_accuracy'].mean()
 #classification report
 from sklearn.metrics import classification_report
 
-print(classification_report(y_test_soham, predicted, target_names=['Died', 'Survived']))
+print(classification_report(y_test_nimishtitanic_nimish, predicted, target_names=['Died', 'Survived']))
 
 
 # In[185]:
@@ -270,8 +270,8 @@ print(classification_report(y_test_soham, predicted, target_names=['Died', 'Surv
 prob = probs[:, 1]
 prob_df = pd.DataFrame(prob)
 prob_df['predict'] = np.where(prob_df[0] >= 0.05, 1, 0)
-y_test_soham = (y_test_soham == 'yes').astype(int)
-Y_A = y_test_soham.values
+y_test_nimishtitanic_nimish = (y_test_nimishtitanic_nimish == 'yes').astype(int)
+Y_A = y_test_nimishtitanic_nimish.values
 Y_P = np.array(prob_df['predict'])
 
 
